7d083e2c0755df09c1ef2498f1d1e972b4b2da97
Russ White <russ.white@lifeway.com>
2018-12-05 10:47:03 -0600

    further cleanup

--- src/test/scala/advent/of/code/Day5.scala
+++ src/test/scala/advent/of/code/Day5.scala
@@ -3,36 +3,28 @@ package advent.of.code
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.annotation.tailrec
-import scala.collection.mutable.ArrayBuffer
 
 class Day5 extends FlatSpec with Matchers with Day {
-
   override def dayNumber: Int = 5
 
-  def willReact(a: Char, b: Char): Boolean = Math.abs(a - b) == 32
-
-
   @tailrec
-  final def part1(s: String):Int = {
-    val res = s.foldLeft(""){case (s,c) =>
-      if (s.isEmpty) s + c
-      else if (willReact(s.last,c)) {
-        s.init
-      }
-      else s + c
+  final def part1(str: String): Int = {
+    def willReact(a: Char, b: Char): Boolean = Math.abs(a - b) == 32
+
+    val res = str.foldLeft("") {
+      case (s, c) =>
+        if (s.isEmpty) s + c
+        else if (willReact(s.last, c)) {
+          s.init
+        } else s + c
     }
-    if (res.length != s.length) part1(res) else res.length
+    if (res.length != str.length) part1(res) else res.length
   }
 
-  def part2(s: String): Int = {
-    s.toLowerCase.distinct.sorted
-      .map { c =>
-        c -> part1(s.filterNot(x => x.toLower == c))
-      }
-      .toMap
-      .minBy(_._2)
-      ._2
-  }
+  def part2(s: String): Int =
+    s.toLowerCase.distinct.map { c =>
+      part1(s.filterNot(x => x.toLower == c))
+    }.min
 
   "reaction" should "react" in {
     part1("dabAcCaCBAcCcaDA") should be(10)
