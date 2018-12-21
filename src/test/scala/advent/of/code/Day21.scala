f3a002b8078499cd96b9cb9134aed96aca5ca66b
Russ White <russ.white@lifeway.com>
2018-12-21 12:26:46 -0600

    optimize day21

--- src/test/scala/advent/of/code/Day21.scala
+++ src/test/scala/advent/of/code/Day21.scala
@@ -3,14 +3,11 @@ import scala.collection.mutable
 
 class Day21 extends Day(21) {
   // I went straight to reverse engineering because I didn't want to waste time on the faulty device :)
-  def solutions: Seq[Int] = {
+  lazy val solutions: Seq[Int] = {
     // need to preserve insert order
     val rs: mutable.LinkedHashSet[Int] = mutable.LinkedHashSet.empty
-    val r0                             = 0
-    var r4                             = 0
-    var r1                             = 0
-    var r1gt255                        = true
-    var adding                         = true
+    var r1, r4                         = 0
+    var r1gt255, adding                = true
     do {
       r1 = r4 | 65536
       r4 = 16031208
@@ -23,7 +20,7 @@ class Day21 extends Day(21) {
         r1 /= 256
       } while (r1gt255)
       adding = rs.add(r4)
-    } while (r4 != r0 && adding)
+    } while (r4 != 0 && adding)
     rs.toSeq
   }
   solutions.head should be(10720163)
