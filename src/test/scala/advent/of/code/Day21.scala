d0f6395e71ced7c4bd3ca92ad055f6dabb1e47cf
Russ White <russ.white@lifeway.com>
2018-12-21 12:23:16 -0600

    cleanup and add input

--- src/test/scala/advent/of/code/Day21.scala
+++ src/test/scala/advent/of/code/Day21.scala
@@ -4,25 +4,24 @@ import scala.collection.mutable
 class Day21 extends Day(21) {
   // I went straight to reverse engineering because I didn't want to waste time on the faulty device :)
   def solutions: Seq[Int] = {
+    // need to preserve insert order
     val rs: mutable.LinkedHashSet[Int] = mutable.LinkedHashSet.empty
     val r0                             = 0
     var r4                             = 0
-    var r2                             = 0
     var r1                             = 0
-    var lte256                         = true
+    var r1gt255                        = true
     var adding                         = true
     do {
       r1 = r4 | 65536
       r4 = 16031208
       do {
-        r2 = r1 & 255
-        r4 += r2
+        r4 += r1 & 255
         r4 &= 16777215
         r4 *= 65899
         r4 &= 16777215
-        lte256 = 256 <= r1
-        if (lte256) r1 /= 256
-      } while (lte256)
+        r1gt255 = r1 > 255
+        r1 /= 256
+      } while (r1gt255)
       adding = rs.add(r4)
     } while (r4 != r0 && adding)
     rs.toSeq
