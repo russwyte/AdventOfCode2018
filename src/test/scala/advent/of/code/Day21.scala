0dbe6541f5df657719823a9b9044d15945ad4522
Russ White <russ.white@lifeway.com>
2018-12-21 12:30:58 -0600

    optimize day21 more

--- src/test/scala/advent/of/code/Day21.scala
+++ src/test/scala/advent/of/code/Day21.scala
@@ -7,7 +7,7 @@ class Day21 extends Day(21) {
     // need to preserve insert order
     val rs: mutable.LinkedHashSet[Int] = mutable.LinkedHashSet.empty
     var r1, r4                         = 0
-    var r1gt255, adding                = true
+    var r1gt255, added                 = true
     do {
       r1 = r4 | 65536
       r4 = 16031208
@@ -19,8 +19,8 @@ class Day21 extends Day(21) {
         r1gt255 = r1 > 255
         r1 /= 256
       } while (r1gt255)
-      adding = rs.add(r4)
-    } while (r4 != 0 && adding)
+      added = rs.add(r4)
+    } while (r4 != 0 && added)
     rs.toSeq
   }
   solutions.head should be(10720163)
