cfdf998569f85c1a517e5c4b29738809b966aea7
Russ White <russ.white@lifeway.com>
2018-12-25 11:58:36 -0600

    cleanup

--- src/test/scala/advent/of/code/Day25.scala
+++ src/test/scala/advent/of/code/Day25.scala
@@ -5,27 +5,15 @@ import graph._
 
 class Day25 extends Day(25) {
 
-  def inRange(p1: Point, p2: Point): Boolean = {
-    (p1 mDistance p2) <= 3
-  }
-
-  def pStars[_: P]: P[Point] = P(pInt ~ "," ~ pInt ~ "," ~ pInt ~ "," ~ pInt).map {
+  def pPoint[_: P]: P[Point] = P(pInt ~ "," ~ pInt ~ "," ~ pInt ~ "," ~ pInt).map {
     case (x, y, z, t) => Point(x, y, z, t)
   }
 
-  def stars(s: String): List[Point] = {
-    s.linesIterator.map(l => parse(l.trim, pStars(_)).get.value).toList
-  }
+  def points(s: String): List[Point] = s.linesIterator.map(l => parse(l.trim, pPoint(_)).get.value).toList
 
-  def graph(stars: List[Point]): GraphMap[Point] =
-    stars
-      .map(b1 => {
-        b1 -> stars.filter(b2 => b2 != b1 && inRange(b1, b2))
-      })
-      .toMap
+  def inRange(p1: Point, p2: Point): Boolean = (p1 mDistance p2) <= 3
 
-  val Sample1 = "0,0,0,0\n 3,0,0,0\n 0,3,0,0\n 0,0,3,0\n 0,0,0,3\n 0,0,0,6\n 9,0,0,0\n12,0,0,0"
-  val Sample2 = "-1,2,2,0\n0,0,2,-2\n0,0,0,-2\n-1,2,0,0\n-2,-2,-2,2\n3,0,2,-1\n-1,3,2,2\n-1,0,-1,0\n0,2,1,-2\n3,0,0,0"
+  def graph(ps: List[Point]): GraphMap[Point] = ps.map(a => a -> ps.filter(b => a != b && inRange(a, b))).toMap
 
   def constellations(s: String): Int = {
     def inner(graph: GraphMap[Point], acc: Int): Int = {
@@ -35,8 +23,11 @@ class Day25 extends Day(25) {
         inner(graph -- found, acc + 1)
       }
     }
-    inner(graph(stars(s)), 0)
+    inner(graph(points(s)), 0)
   }
+
+  val Sample1 = "0,0,0,0\n 3,0,0,0\n 0,3,0,0\n 0,0,3,0\n 0,0,0,3\n 0,0,0,6\n 9,0,0,0\n12,0,0,0"
+  val Sample2 = "-1,2,2,0\n0,0,2,-2\n0,0,0,-2\n-1,2,0,0\n-2,-2,-2,2\n3,0,2,-1\n-1,3,2,2\n-1,0,-1,0\n0,2,1,-2\n3,0,0,0"
 
   constellations(Sample1) should be(2)
   constellations(Sample2) should be(4)
